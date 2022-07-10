88f61fa89bb04fe6e29798ab4609ced56a01604e
dhlee <dhlee3994@gmail.com>
2022-07-10 23:20:23 +0900

    권한별 허용페이지 설정

---
+++ src/main/java/my/study/corespringsecurity/security/configs/SecurityConfig.java
@@ -0,0 +1,23 @@
+package my.study.corespringsecurity.security.configs;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/mypage").hasRole("USER")
+                .antMatchers("/messages").hasRole("MANAGER")
+                .antMatchers("/config").hasRole("ADMIN")
+                .anyRequest().authenticated();
+
+        http
+                .formLogin();
+    }
+}

139a9ad02dc0d0b3b24d2775231baf81a7e911c8
dhlee <dhlee3994@gmail.com>
2022-07-14 23:15:19 +0900

    기본 유저 세팅

--- src/main/java/my/study/corespringsecurity/CorespringsecurityApplication.java
+++ src/main/java/my/study/corespringsecurity/CorespringsecurityApplication.java
@@ -1,7 +1,16 @@
 package my.study.corespringsecurity;
 
+import my.study.corespringsecurity.domain.Account;
+import my.study.corespringsecurity.domain.AccountDto;
+import my.study.corespringsecurity.domain.mapper.AccountMapper;
+import my.study.corespringsecurity.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import static my.study.corespringsecurity.domain.mapper.AccountMapper.INSTANCE;
 
 @SpringBootApplication
 public class CorespringsecurityApplication {
@@ -10,4 +19,13 @@ public class CorespringsecurityApplication {
         SpringApplication.run(CorespringsecurityApplication.class, args);
     }
 
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void createBasicAccounts() {
+        accountRepository.save(INSTANCE.toEntity(new AccountDto("user", "aa", "", "", "USER")));
+        accountRepository.save(INSTANCE.toEntity(new AccountDto("manager", "aa", "", "", "MANAGER")));
+        accountRepository.save(INSTANCE.toEntity(new AccountDto("admin", "aa", "", "", "ADMIN")));
+    }
 }

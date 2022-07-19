e4dbb0e1118b87f7310f60f73ce9402b9dc2dddd
dhlee <dhlee3994@gmail.com>
2022-07-19 23:45:51 +0900

    Ajax 인증 처리자

--- src/main/java/my/study/corespringsecurity/security/configs/AjaxSecurityConfig.java
+++ src/main/java/my/study/corespringsecurity/security/configs/AjaxSecurityConfig.java
@@ -1,16 +1,27 @@
 package my.study.corespringsecurity.security.configs;
 
+import lombok.RequiredArgsConstructor;
 import my.study.corespringsecurity.security.filter.AjaxLoginProcessingFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@RequiredArgsConstructor
 @Order(0)
 @EnableWebSecurity
 public class AjaxSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final AuthenticationProvider ajaxAuthenticationProvider;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(ajaxAuthenticationProvider);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
